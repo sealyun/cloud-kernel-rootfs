06129d6d474a146fb892cf6c95d1215ecc87c50a
cuisongliu <cuisongliu@qq.com>
2021-07-16 17:14:23 +0800

    first commit

--- pkg/build/docker.go
+++ pkg/build/docker.go
@@ -19,7 +19,7 @@ mv ossutil64 /usr/bin/ && \
 yum install -y git conntrack tree && \
 git clone https://github.com/alibaba/sealer && \
 git clone https://github.com/sealyun/cloud-kernel-rootfs && mv cloud-kernel-rootfs cloud-kernel && \
-cd cloud-kernel && git checkout %s && mkdir -p rootfs && mkdir -p rootfs/bin &&  mkdir -p rootfs/registry \
+cd cloud-kernel && git checkout %s && mkdir -p rootfs && mkdir -p rootfs/bin &&  mkdir -p rootfs/registry && \
 cp -rf runtime/rootfs/* rootfs/ && cp -rf runtime/docker/* rootfs/   && \
 cp -rf ../sealer/rootfs/rootfs/* rootfs/ && cp -rf ../sealer/rootfs/docker/* rootfs/   && \
 %s && \
